2cc49c9193d7042544c080ca068966652e0560e0
whatever823301233 <xq823301233@sina.com>
2016-06-22 23:27:04 +0800

    修改主页界面

--- app/src/main/java/com/qiang/meidaproject/entity/BaseEntity.java
+++ app/src/main/java/com/qiang/meidaproject/entity/BaseEntity.java
@@ -6,6 +6,6 @@ import java.io.Serializable;
  * Created by Qiang on 2016/3/24.
  *
  */
-public class BaseEntity implements Serializable {
+public abstract class BaseEntity implements Serializable {
 
 }
